83560170d037d27ce65ac06375d8db9470c6ebc4
Yikch <99170695+Yikch@users.noreply.github.com>
2023-03-06 14:02:04 +0100

    Add files via upload
    Subido plantilla de la P2

---
+++ P2/mysql/datos.sql
@@ -0,0 +1,25 @@
+/*
+  Recuerda que deshabilitar la opción "Enable foreign key checks" para evitar problemas a la hora de importar el script.
+*/
+TRUNCATE TABLE `RolesUsuario`;
+TRUNCATE TABLE `Roles`;
+TRUNCATE TABLE `Usuarios`;
+
+INSERT INTO `Roles` (`id`, `nombre`) VALUES
+(1, 'admin'),
+(2, 'user');
+
+
+INSERT INTO `RolesUsuario` (`usuario`, `rol`) VALUES
+(1, 1),
+(1, 2),
+(2, 2);
+
+/*
+  user: userpass
+  admin: adminpass
+*/
+INSERT INTO `Usuarios` (`id`, `nombreUsuario`, `nombre`, `password`) VALUES
+(1, 'admin', 'Administrador', '$2y$10$O3c1kBFa2yDK5F47IUqusOJmIANjHP6EiPyke5dD18ldJEow.e0eS'),
+(2, 'user', 'Usuario', '$2y$10$uM6NtF.f6e.1Ffu2rMWYV.j.X8lhWq9l8PwJcs9/ioVKTGqink6DG');
+
