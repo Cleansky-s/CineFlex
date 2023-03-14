3417e56e18481f6f326ac1c16ce157bae816b658
javier026 <76840505+javier026@users.noreply.github.com>
2023-03-14 21:02:50 +0100

    Add files via upload
    Lo que hacia falta añadir que esta en el txt, creo que debería funcionar

--- pract2/includes/vistas/comun/cabecera.php
+++ pract2/includes/vistas/comun/cabecera.php
@@ -1,7 +1,6 @@
 <?php
-
-require_once __DIR__.'/../helpers/usuarios.php';
-
+	session_start();
+	require_once __DIR__.'/../helpers/usuarios.php';
 ?>
 <header>
 	<div>
@@ -10,8 +9,13 @@ require_once __DIR__.'/../helpers/usuarios.php';
 		</a>
 		<a href="index.php"> Películas </a>
 		<a href="cines_cartelera.php"> Cines/Cartelera </a>
-		<?= proveedor() ?>
-		<?= admin() ?>
+		<?php
+			//if(isset($_SESSION['login']) && (isset($_SESSION['esAdmin']))) {
+				echo '<a href="proveerPeliculas.php"> Proveer peliculas </a>
+				      <a href="proveerCines.php"> Proveer Cines </a>';
+			//}
+		?>
+		
 		<a href="biblioteca.php"> Biblioteca </a>
 		<?= saludo() ?>
 	</div>
